ca55e7370195d0bc7661035e8db5dc1f47d24cde
Joe <joe.leong@binfinite.com.my>
2021-07-05 21:21:08 +0800

    fixed

--- mod5/js/script.js
+++ mod5/js/script.js
@@ -103,7 +103,6 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories);
 
-
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -116,8 +115,8 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage = chosenCategoryShortName
-      homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", homeHtmlToInsertIntoMainPage);
-
+      homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName","'" + homeHtmlToInsertIntoMainPage["short_name"] + "'");
+      
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
